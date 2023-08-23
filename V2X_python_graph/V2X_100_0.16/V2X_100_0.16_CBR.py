a747f2cf8b7ca72c2f925ca83995fb42ed40c678
Taekyung Kim <taekyungkim@Taekyungs-MacBook-Pro.local>
2023-08-23 12:04:05 +0900

    first commit

---
+++ V2X_python_graph/V2X_100_0.16/V2X_100_0.16_CBR.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+
+data_f = open("V2X_100_0.16.csv",'r', encoding='utf-8-sig')
+
+BSM_TIMEs = []
+CBRs = []
+WSA_recieved_TIMEs = []
+ITTs = []
+
+for line in data_f:
+    (bsm_time, cbr, wsa_recieved_time, itt) = line.split(',')
+    BSM_TIMEs.append(bsm_time)
+    CBRs.append(cbr)
+    WSA_recieved_TIMEs.append(wsa_recieved_time)
+    ITTs.append(itt)
+    
+data_f.close()
+
+BSM_TIMEs = [float (i) for i in BSM_TIMEs]
+CBRs = [float (i) for i in CBRs]
+WSA_recieved_TIMEs = [float (i) for i in WSA_recieved_TIMEs]
+WSA_recieved_TIMEs = [i-1 for i in WSA_recieved_TIMEs]
+ITTs = [float (i) for i in ITTs]
+
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+
+plt.plot(BSM_TIMEs, CBRs, color='magenta', marker='o', linestyle='dashed', label='CBR')
+plt.title("CBR stabilizing", fontsize=20)
+plt.xlabel("TIME", fontsize=16)
+plt.ylabel("CBR", fontsize=16)
+plt.grid(True)
+plt.legend(loc='upper left', fontsize=16)
+plt.savefig('V2X_100_0.16_CBR.png')
